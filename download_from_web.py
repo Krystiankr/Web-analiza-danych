5a103d5447f9baf870ae08ce8fe5111305b24b01
Krystian Kręcik <56454269+Krystiankr@users.noreply.github.com>
2022-02-12 22:39:16 +0100

    Update download_from_web.py
    Downloading files with using recursion

--- download_from_web.py
+++ download_from_web.py
@@ -1,46 +1,50 @@
+from typing import List
+from tqdm import tqdm
+import requests
 import os
 import re
-import time
-
-import requests
 from bs4 import BeautifulSoup
 
-destination = r"C:\Users..."
-source_url = "http://wmii.uwm.edu.pl..."
+def get_all_dirs(url) -> List[str]:
+    def format_name(name):
+        parent_text = name.parent.parent.text
+        parent_text = parent_text.split('/')[0]
+        return parent_text
+    return [format_name(dir_name) for dir_name in url.findAll("img", {"alt": "[DIR]"})]
+
+def get_all_files(url) -> List[str]:
+    return [href_child['href'] for href_child in url.findAll(href=re.compile(allows_extension))]
+
+def recursion_dir(source_url: str, destination: str, show_progress=True):
+    r = requests.get(source_url)
+    r.encoding = r.apparent_encoding
+    doc = BeautifulSoup(r.text, "html.parser")
+    with tqdm(total=1, desc="Searching", unit="file", disable=not show_progress) as myprogressbar:
+        all_files = get_all_files(doc)
+        myprogressbar.total += len(all_files) - 1
+        for file in all_files:
+            myprogressbar.update()
+            full_link = rf"{source_url}//{file}"
+            where_save = f"{destination}\\{file}"
+            r = requests.get(full_link, allow_redirects=True)
+            with open(where_save, 'wb') as f:
+                f.write(r.content)
+    for sub_dir in get_all_dirs(doc):
+        path = os.path.join(destination, sub_dir)
+        # Check if dir exists
+        if not os.path.isdir(f"{destination}\{sub_dir}"):
+            print(f"Create new directory {sub_dir}")
+            os.mkdir(path)
+        else:
+            print(f"{sub_dir} folder already exists.")
+        full_link = rf"{source_url}{sub_dir}"
+        dest = f"{destination}\\{sub_dir}"
+        print(f"full {full_link}")
+        recursion_dir(full_link, dest)
+    print("-------------Koniec-------------")
+    
+destination = r"C:\Users\..."
+source_url = "http://wmii.uwm.edu.pl/...."
 allows_extension = "\."
 
-r = requests.get(source_url)
-r.encoding = r.apparent_encoding
-doc = BeautifulSoup(r.text, "html.parser")
-
-print("Displaying folders in this directory.")
-for el in doc.findAll("img", {"alt": "[DIR]"}):
-    parent_text: str = el.parent.parent.text
-    parent_text = parent_text.split('/')[0]
-    print(parent_text)
-    print(f"Opening next directory {parent_text}")
-    path = os.path.join(destination, parent_text)
-
-    # Check if dir exists
-    if not os.path.isdir(f"{destination}\{parent_text}"):
-        print(f"Create new directory {parent_text}")
-        os.mkdir(path)
-    else:
-        print(f"{parent_text} folder already exists.")
-
-    child_link = f"{source_url}/{parent_text}"
-    r = requests.get(child_link)
-    r.encoding = r.apparent_encoding
-    child_doc = BeautifulSoup(r.text, "html.parser")
-    for href_child in child_doc.findAll(href=re.compile(allows_extension)):
-        print(href_child['href'])
-        full_link = rf"{source_url}//{parent_text}//{href_child['href']}"
-        print(f"Full link = {full_link}")
-        where_save = f"{destination}\\{parent_text}\\{href_child['href']}"
-        print(f"Where save {where_save}")
-        r = requests.get(full_link, allow_redirects=True)
-        with open(where_save, 'wb') as f:
-            f.write(r.content)
-        # open(where_save, 'wb').write(r.content)
-    print()
-print("-------------Koniec-------------")
+recursion_dir(source_url, destination)
